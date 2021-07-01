3e0ce60265155569caad67ae388deef4bf818375
tatichan <tsalazarp.uq@gmail.com>
2021-07-01 10:25:49 -0500

    script generado desde mysql

--- vuelosbd.sql
+++ vuelosbd.sql
@@ -1,123 +1,280 @@
--- create database vuelopruebadb;
-use vuelopruebadb;
+-- create database vuelobd;
+use vuelobd;
 
--- CREATE TABLE aeropuerto (
---    id_aeropuerto      INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
---    nombre_aeropuerto  VARCHAR(50) NOT NULL,
---    numero_de_pistas   INTEGER NOT NULL,
---    aeropuerto_activo  VARCHAR(2) NOT NULL
--- );
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: localhost    Database: vuelopruebadb
+-- ------------------------------------------------------
+-- Server version	8.0.25
 
--- ALTER TABLE aeropuerto ADD CONSTRAINT aeropuerto_pk PRIMARY KEY ( id_aeropuerto );
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE avion (
-    id_avion                  INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    aeropuerto_id_aeropuerto  INTEGER NOT NULL,
-    compania_id_compania      INTEGER NOT NULL
-);
+--
+-- Table structure for table `aeropuerto`
+--
 
--- ALTER TABLE avion ADD CONSTRAINT avion_pk PRIMARY KEY ( id_avion );
+DROP TABLE IF EXISTS `aeropuerto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aeropuerto` (
+  `id_aeropuerto` int NOT NULL AUTO_INCREMENT,
+  `nombre_aeropuerto` varchar(50) NOT NULL,
+  `numero_de_pistas` int NOT NULL,
+  `aeropuerto_activo` varchar(2) NOT NULL,
+  `ciudad` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_aeropuerto`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE compania (
-    id_compania      INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    nombre_compania  VARCHAR(50) NOT NULL
-);
+--
+-- Dumping data for table `aeropuerto`
+--
 
--- ALTER TABLE compania ADD CONSTRAINT compania_pk PRIMARY KEY ( id_compania );
+LOCK TABLES `aeropuerto` WRITE;
+/*!40000 ALTER TABLE `aeropuerto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aeropuerto` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE destino (
-    id_destino         INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    nombre_aeropuerto  VARCHAR(50) NOT NULL,
-    nombre_ciudad      VARCHAR(100) NOT NULL
-);
+--
+-- Table structure for table `avion`
+--
 
--- ALTER TABLE destino ADD CONSTRAINT destino_pk PRIMARY KEY ( id_destino );
+DROP TABLE IF EXISTS `avion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `avion` (
+  `id_avion` int NOT NULL AUTO_INCREMENT,
+  `aeropuerto_id_aeropuerto` int NOT NULL,
+  `compania_id_compania` int NOT NULL,
+  `modelo` varchar(30) DEFAULT NULL,
+  `capacidad` int DEFAULT NULL,
+  PRIMARY KEY (`id_avion`),
+  KEY `avion_aeropuerto_fk` (`aeropuerto_id_aeropuerto`),
+  KEY `avion_compania_fk` (`compania_id_compania`),
+  CONSTRAINT `avion_aeropuerto_fk` FOREIGN KEY (`aeropuerto_id_aeropuerto`) REFERENCES `aeropuerto` (`id_aeropuerto`),
+  CONSTRAINT `avion_compania_fk` FOREIGN KEY (`compania_id_compania`) REFERENCES `compania` (`id_compania`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE itinerario (
-    id_itinerario       INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    fecha_salida        DATE DEFAULT '0000-00-00',
-    fecha_llegada       DATE DEFAULT '0000-00-00',
-    destino_id_destino  INTEGER NOT NULL,
-    origen_id_origen    INTEGER NOT NULL
-);
+--
+-- Dumping data for table `avion`
+--
 
--- TABLE itinerario ADD CONSTRAINT itinerario_pk PRIMARY KEY ( id_itinerario );
+LOCK TABLES `avion` WRITE;
+/*!40000 ALTER TABLE `avion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `avion` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE origen (
-    id_origen          INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    nombre_aeropuerto  VARCHAR(50) NOT NULL,
-    nombre_ciudad      VARCHAR(100) NOT NULL
-);
+--
+-- Table structure for table `compania`
+--
 
--- ALTER TABLE origen ADD CONSTRAINT origen_pk PRIMARY KEY ( id_origen );
+DROP TABLE IF EXISTS `compania`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compania` (
+  `id_compania` int NOT NULL AUTO_INCREMENT,
+  `nombre_compania` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_compania`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE pasaje (
-    id_pasaje       INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    clase           VARCHAR(30) NOT NULL,
-    asiento         VARCHAR(15) NOT NULL,
-    valor           DECIMAL(10,2) NOT NULL,
-    pasajero_id     INTEGER NOT NULL,
-    vuelo_id_vuelo  INTEGER NOT NULL
-);
+--
+-- Dumping data for table `compania`
+--
 
--- ALTER TABLE pasaje ADD CONSTRAINT pasaje_pk PRIMARY KEY ( id_pasaje );
+LOCK TABLES `compania` WRITE;
+/*!40000 ALTER TABLE `compania` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compania` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE pasajero (
-    id         INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    nombre     VARCHAR(30) NOT NULL,
-    documento  VARCHAR(15) NOT NULL,
-    edad       VARCHAR(15) NOT NULL,
-    email      VARCHAR(30) ,
-    telefono   VARCHAR(30) 
-);
+--
+-- Table structure for table `destino`
+--
 
--- ALTER TABLE pasajero ADD CONSTRAINT pasajero_pk PRIMARY KEY ( id );
+DROP TABLE IF EXISTS `destino`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `destino` (
+  `id_destino` int NOT NULL AUTO_INCREMENT,
+  `nombre_aeropuerto` varchar(50) NOT NULL,
+  `nombre_ciudad` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_destino`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE vuelo (
-    id_vuelo                  INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    capacidad                 VARCHAR(10),
-    numerovuelo               VARCHAR(10),
-    compania_id_compania      INTEGER NOT NULL,
-    itinerario_id_itinerario  INTEGER NOT NULL,
-    aeropuerto_id_aeropuerto  INTEGER NOT NULL,
-    avion_id_avion            INTEGER NOT NULL
-);
+--
+-- Dumping data for table `destino`
+--
 
--- ALTER TABLE vuelo ADD CONSTRAINT vuelo_pk PRIMARY KEY ( id_vuelo );
+LOCK TABLES `destino` WRITE;
+/*!40000 ALTER TABLE `destino` DISABLE KEYS */;
+/*!40000 ALTER TABLE `destino` ENABLE KEYS */;
+UNLOCK TABLES;
 
-ALTER TABLE avion
-    ADD CONSTRAINT avion_aeropuerto_fk FOREIGN KEY ( aeropuerto_id_aeropuerto )
-        REFERENCES aeropuerto ( id_aeropuerto );
+--
+-- Table structure for table `itinerario`
+--
 
-ALTER TABLE avion
-    ADD CONSTRAINT avion_compania_fk FOREIGN KEY ( compania_id_compania )
-        REFERENCES compania ( id_compania );
+DROP TABLE IF EXISTS `itinerario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `itinerario` (
+  `id_itinerario` int NOT NULL AUTO_INCREMENT,
+  `fecha_salida` timestamp NULL DEFAULT NULL,
+  `fecha_llegada` timestamp NULL DEFAULT NULL,
+  `destino_id_destino` int NOT NULL,
+  `origen_id_origen` int NOT NULL,
+  PRIMARY KEY (`id_itinerario`),
+  KEY `itinerario_destino_fk` (`destino_id_destino`),
+  KEY `itinerario_origen_fk` (`origen_id_origen`),
+  CONSTRAINT `itinerario_destino_fk` FOREIGN KEY (`destino_id_destino`) REFERENCES `destino` (`id_destino`),
+  CONSTRAINT `itinerario_origen_fk` FOREIGN KEY (`origen_id_origen`) REFERENCES `origen` (`id_origen`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE itinerario
-    ADD CONSTRAINT itinerario_destino_fk FOREIGN KEY ( destino_id_destino )
-        REFERENCES destino ( id_destino );
+--
+-- Dumping data for table `itinerario`
+--
 
-ALTER TABLE itinerario
-    ADD CONSTRAINT itinerario_origen_fk FOREIGN KEY ( origen_id_origen )
-        REFERENCES origen ( id_origen );
+LOCK TABLES `itinerario` WRITE;
+/*!40000 ALTER TABLE `itinerario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `itinerario` ENABLE KEYS */;
+UNLOCK TABLES;
 
-ALTER TABLE pasaje
-    ADD CONSTRAINT pasaje_pasajero_fk FOREIGN KEY ( pasajero_id )
-        REFERENCES pasajero ( id );
+--
+-- Table structure for table `origen`
+--
 
-ALTER TABLE pasaje
-    ADD CONSTRAINT pasaje_vuelo_fk FOREIGN KEY ( vuelo_id_vuelo )
-        REFERENCES vuelo ( id_vuelo );
+DROP TABLE IF EXISTS `origen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `origen` (
+  `id_origen` int NOT NULL AUTO_INCREMENT,
+  `nombre_aeropuerto` varchar(50) NOT NULL,
+  `nombre_ciudad` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_origen`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE vuelo
-    ADD CONSTRAINT vuelo_avion_fk FOREIGN KEY ( avion_id_avion )
-        REFERENCES avion ( id_avion );
+--
+-- Dumping data for table `origen`
+--
 
-ALTER TABLE vuelo
-    ADD CONSTRAINT vuelo_compania_fk FOREIGN KEY ( compania_id_compania )
-        REFERENCES compania ( id_compania );
+LOCK TABLES `origen` WRITE;
+/*!40000 ALTER TABLE `origen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `origen` ENABLE KEYS */;
+UNLOCK TABLES;
 
-ALTER TABLE vuelo
-    ADD CONSTRAINT vuelo_itinerario_fk FOREIGN KEY ( itinerario_id_itinerario )
-        REFERENCES itinerario ( id_itinerario );
+--
+-- Table structure for table `pasaje`
+--
+
+DROP TABLE IF EXISTS `pasaje`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pasaje` (
+  `id_pasaje` int NOT NULL AUTO_INCREMENT,
+  `clase` varchar(30) NOT NULL,
+  `asiento` varchar(15) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `pasajero_id` int NOT NULL,
+  `vuelo_id_vuelo` int NOT NULL,
+  PRIMARY KEY (`id_pasaje`),
+  KEY `pasaje_pasajero_fk` (`pasajero_id`),
+  KEY `pasaje_vuelo_fk` (`vuelo_id_vuelo`),
+  CONSTRAINT `pasaje_pasajero_fk` FOREIGN KEY (`pasajero_id`) REFERENCES `pasajero` (`id`),
+  CONSTRAINT `pasaje_vuelo_fk` FOREIGN KEY (`vuelo_id_vuelo`) REFERENCES `vuelo` (`id_vuelo`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pasaje`
+--
+
+LOCK TABLES `pasaje` WRITE;
+/*!40000 ALTER TABLE `pasaje` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pasaje` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pasajero`
+--
+
+DROP TABLE IF EXISTS `pasajero`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pasajero` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) NOT NULL,
+  `documento` varchar(15) NOT NULL,
+  `edad` varchar(15) NOT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `telefono` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pasajero`
+--
+
+LOCK TABLES `pasajero` WRITE;
+/*!40000 ALTER TABLE `pasajero` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pasajero` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vuelo`
+--
+
+DROP TABLE IF EXISTS `vuelo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vuelo` (
+  `id_vuelo` int NOT NULL AUTO_INCREMENT,
+  `capacidad` varchar(10) DEFAULT NULL,
+  `numerovuelo` varchar(10) DEFAULT NULL,
+  `compania_id_compania` int NOT NULL,
+  `itinerario_id_itinerario` int NOT NULL,
+  `avion_id_avion` int NOT NULL,
+  PRIMARY KEY (`id_vuelo`),
+  KEY `vuelo_avion_fk` (`avion_id_avion`),
+  KEY `vuelo_compania_fk` (`compania_id_compania`),
+  KEY `vuelo_itinerario_fk` (`itinerario_id_itinerario`),
+  CONSTRAINT `vuelo_avion_fk` FOREIGN KEY (`avion_id_avion`) REFERENCES `avion` (`id_avion`),
+  CONSTRAINT `vuelo_compania_fk` FOREIGN KEY (`compania_id_compania`) REFERENCES `compania` (`id_compania`),
+  CONSTRAINT `vuelo_itinerario_fk` FOREIGN KEY (`itinerario_id_itinerario`) REFERENCES `itinerario` (`id_itinerario`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vuelo`
+--
+
+LOCK TABLES `vuelo` WRITE;
+/*!40000 ALTER TABLE `vuelo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vuelo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-30  8:38:49
+
 
